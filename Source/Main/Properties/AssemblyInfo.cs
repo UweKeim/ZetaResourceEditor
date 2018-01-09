f7e3ff178836f1be86d63e27a0ebef92b2ce56ec
Uwe Keim <uwe.keim@gmail.com>
2018-01-09 04:49:19 +0100

    Updated libraries
    .

--- Source/Main/Properties/AssemblyInfo.cs
+++ Source/Main/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.0.16")]
-[assembly: AssemblyFileVersion("2.3.0.16")]
+[assembly: AssemblyVersion("2.3.0.17")]
+[assembly: AssemblyFileVersion("2.3.0.17")]
