c8ebbd0cd18c829c4441feb9d47dcb9ca3b24418
Uwe Keim <uwe.keim@zeta-software.de>
2022-01-14 08:43:54 +0100

    Mirrored from local repository @ 01/14/2022 08:43

--- Source/Main/Properties/AssemblyInfo.cs
+++ Source/Main/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.4.33")]
-[assembly: AssemblyFileVersion("2.3.4.33")]
+[assembly: AssemblyVersion("2.3.5.33")]
+[assembly: AssemblyFileVersion("2.3.5.33")]
