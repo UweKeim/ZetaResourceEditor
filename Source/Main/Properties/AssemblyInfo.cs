891043241dcb8d0045b85f37afe78c40761c9f56
Uwe Keim <uwe.keim@zeta-software.de>
2023-01-29 12:19:50 +0100

    Mirrored from local repository @ 01/29/2023 12:19

---
+++ Source/Main/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Zeta Resource Editor")]
+[assembly: AssemblyDescription("Free tool for editing string resources inside multiple resource files in parallel")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zeta Software GmbH")]
+[assembly: AssemblyProduct("Zeta Resource Editor")]
+[assembly: AssemblyCopyright("Copyright 2006-2023 Zeta Software GmbH")]
+[assembly: AssemblyTrademark("https://www.zeta-resource-editor.com")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4303abd0-1051-43a2-a3c2-faaca9411fcd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.3.36.0")]
+[assembly: AssemblyFileVersion("2.3.36.0")]
